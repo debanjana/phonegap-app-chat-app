3bdaebe2d1ea3c88da966b393b3fb9624748dde3
unknown <sbose@IBM-SBOSE-BL.fiberlink.local>
2015-04-07 11:15:18 +0530

    prevented auto form submit

--- www/js/index.js
+++ www/js/index.js
@@ -65,8 +65,10 @@ var onReady = function(){
 };
 // this is for the Enter key press
 $('#messageText').keypress(function(event){
+
     var keycode = (event.keyCode ? event.keyCode : event.which);
     if(keycode == '13'){
+         event.preventDefault();
          onMessageSend();      
     }
 });
